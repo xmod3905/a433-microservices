d9c7a9449727116b60a51d36a82d192022d63f5a
Muhammad Iqbal <muh.iqbalg1@gmail.com>
2026-05-11 23:28:54 +0700

    feat: build script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Membuild Docker Image dengan nama karsajobs dan tag latest secara lokal
+docker build -t ghcr.io/xmod3905/karsajobs:latest .
+
+# Login ke Github Packages (Baca dari Host Env TOKEN GITHUB)
+echo $TOKEN_GITHUB | docker login ghcr.io -u xmod3905 --password-stdin
+
+# Mengunggah image ke Github Packages
+docker push ghcr.io/xmod3905/karsajobs:latest
